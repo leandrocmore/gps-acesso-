208198cc9367642eca37a443597fe3f44e27f304
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-03-25 13:35:24 -0300

    teste

--- ProjetoApp/settings.py
+++ ProjetoApp/settings.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AppProjeto',
+    'AppFronteEnd',
     'AppProjeto.apps.AppProjetoConf',
     'AppFronteEnd.apps.AppFronteEndConf', 
     'rest_framework',
