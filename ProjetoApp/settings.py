3419442ebc248958cfcaeb6eaf14e906090c49e3
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-03-25 14:02:05 -0300

    teste

--- ProjetoApp/settings.py
+++ ProjetoApp/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'ProjetoApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASE_URL = os.environ.get('DATABASE_URL' )
+"""DATABASE_URL = os.environ.get('DATABASE_URL' )
 DATABASES = {
     'default': dj_database_url.parse(DATABASE_URL)
-}
+}"""
 
 
 # Password validation
