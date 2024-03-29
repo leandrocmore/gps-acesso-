5bb1f9b5ff04f76b72f2a593a42ab42aa1ffba47
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-03-29 19:01:40 -0300

    teste

--- AppDirecionada/static/direct/direct.js
+++ AppDirecionada/static/direct/direct.js
@@ -5,9 +5,24 @@
 ];
 function redirecionarParaSiteAleatorio() {
     
-    var siteAleatorio = sites[Math.floor(Math.random() * sites.length)];
+    let siteAleatorio = sites[Math.floor(Math.random() * sites.length)];
    
     window.location.href = siteAleatorio;
+
+    function obterLocalizacao() {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+    
+    function success(position) {
+        console.log('Localização obtida:', position);
+    }
+    
+    function error() {
+        console.error('Falha ao obter a localização do usuário.');
+    }
+    
+   
+    obterLocalizacao();
 }
 
 redirecionarParaSiteAleatorio();
