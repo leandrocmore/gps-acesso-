d75010c9384a54369d97bd07bb092a6e955a4ddf
leandro camargo more <112519932+leandrocmore@users.noreply.github.com>
2024-03-29 19:11:47 -0300

    Update direct.js

--- staticfiles/direct/direct.js
+++ staticfiles/direct/direct.js
@@ -1,13 +1,28 @@
 
    let sites = [
-    "https://www.google.com",
+    console.log(obterLocalizacao())
   
 ];
 function redirecionarParaSiteAleatorio() {
     
-    var siteAleatorio = sites[Math.floor(Math.random() * sites.length)];
+    let siteAleatorio = sites[Math.floor(Math.random() * sites.length)];
    
     window.location.href = siteAleatorio;
+
+    function obterLocalizacao() {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+    
+    function success(position) {
+        console.log('Localização obtida:', position);
+    }
+    
+    function error() {
+        console.error('Falha ao obter a localização do usuário.');
+    }
+    
+   
+    obterLocalizacao();
 }
 
 redirecionarParaSiteAleatorio();
